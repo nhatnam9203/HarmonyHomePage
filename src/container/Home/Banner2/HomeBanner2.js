351db0a1bb050f2e5811e36a34bf4d3874c68c0f
tuloki <nguyendanhtu789@gmail.com>
2020-12-14 17:07:56 +0700

    add modal

--- src/container/Home/Banner2/HomeBanner2.js
+++ src/container/Home/Banner2/HomeBanner2.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
+import Modal from "react-bootstrap/Modal";
 
 export default function HomeBanner2() {
+  const [show, setShow] = useState(false);
+
   return (
     <main className="home_banner2 d-flex flex-column justify-content-start align-items-center w-100">
       <h1 className="home_banner2-title text-uppercase font-weight-bold text-center">
@@ -12,7 +15,29 @@ export default function HomeBanner2() {
         each other through an API (Application Programing Interface), allowing
         for mobile interactive booking, On-demand marketing, and mobile payment.
       </p>
-      <AiFillPlayCircle className="home_banner2-icon" />
+      <button className="border-0" onClick={() => setShow(true)}>
+        <AiFillPlayCircle className="home_banner2-icon" />
+      </button>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        size="xl"
+        aria-labelledby="video-modal"
+        centered
+        className="video-modal text-center"
+      >
+        <Modal.Body>
+          <iframe
+            title="HarmonyPay Salon POS"
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/UgJuD4ZC1Lk"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Modal.Body>
+      </Modal>
     </main>
   );
 }
