b32d01e6eb2f78896cb53a1b4f847e7debfce43d
nam.phan@levinci <nam.phan@levinci.com>
2021-12-27 10:54:26 +0700

    update UI

--- src/container/Pricing/Package/Package.js
+++ src/container/Pricing/Package/Package.js
@@ -35,6 +35,7 @@ export default function Package() {
     packageList[1]?.pricing * packageList[1]?.annually;
 
   const pricingAnnuallyPro = packageList[0]?.pricing * packageList[0]?.annually;
+
   const pricingAnnuallyBasic1 = pricingAnnuallyBasic.toFixed(2);
   const pricingAnnuallyMedium1 = pricingAnnuallyMedium.toFixed(2);
   const pricingAnnuallyPro1 = pricingAnnuallyPro.toFixed(2);
@@ -53,12 +54,151 @@ export default function Package() {
           </h6>
         </div>
 
+        <div className="package__subItem">
+          <h2 className="text-center font-weight-bold">
+            HarmonyPay POS Salon
+          </h2>
+          <div className="d-flex align-items-center package__subItem">
+            {checked ? (
+              <span className="text-center mr-lg-3 package__features--span">
+                Billed
+                <br /> Monthly
+              </span>
+            ) : (
+              <span className="text-center mr-lg-3 package__features--span checked">
+                Billed
+                <br /> Monthly
+              </span>
+            )}
+
+            <input
+              type="checkbox"
+              className="switch mr-lg-3 switch_price"
+              onClick={handleChecked}
+            />
+            {checked ? (
+              <span className="text-center package__features--span checked">
+                Billed
+                <br /> Annually
+              </span>
+            ) : (
+              <span className="text-center package__features--span">
+                Billed
+                <br /> Annually
+              </span>
+            )}
+          </div>
+        </div>
+
         <Row className="package__container mx-auto ">
-          <div className="package__col">
+
+          <Col
+            xs={12}
+            md={4}
+            className=""
+          >
+            <Card className="package__item">
+              <HeaderPackage
+                title="LEGEND"
+                subTitle="8 Staffs"
+                color="#1266AE"
+                isSpecial={false}
+              />
+              <Card.Body className="package__item--cardbody d-flex flex-column">
+                <Card.Title className="package__item--title text-center font-weight-bold">
+                  ${checked ? pricingAnnuallyBasic1 : packageList[2]?.pricing}
+                  <span className="package__item--sub">
+                    /{checked ? "year" : "month"}
+                  </span>
+                </Card.Title>
+                <Link
+                  className="package__item--btn text-center"
+                  to="/home/sign-up"
+                >
+                  Start free trial
+                </Link>
+                <Card.Text className="package__item--text">
+                  The legendary struggle of a salon owner without Harmony POS
+                  system. Let’s this package mark the beginning of your
+                  legendary successful salon.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col
+            xs={12}
+            md={4}
+            className=""
+          >
+            <Card className="package__item">
+              <HeaderPackage
+                title="LIVE, LOVE AND ENJOY"
+                subTitle="15 Staffs"
+                color="#0E5999"
+                isSpecial={true}
+              />
+              <Card.Body className="package__item--cardbody d-flex flex-column">
+                <Card.Title className="package__item--title text-center font-weight-bold">
+                  ${checked ? pricingAnnuallyMedium1 : packageList[1]?.pricing}
+                  <span className="package__item--sub">
+                    /{checked ? "year" : "month"}
+                  </span>
+                </Card.Title>
+                <Link
+                  className="package__item--btn text-center"
+                  to="/home/sign-up"
+                >
+                  Start free trial
+                </Link>
+                <Card.Text className="package__item--text">
+                  The purpose of life is to live it, to experience and enjoy the
+                  love it gives in every breath. Let live, love, and enjoy life
+                  as it comes.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col
+            xs={12}
+            md={4}
+            className=""
+          >
+            <Card className="package__item">
+              <HeaderPackage
+                title="THE QUAN"
+                subTitle="16+ Staffs"
+                color="#032E53"
+                isSpecial={true}
+              />
+              <Card.Body className="package__item--cardbody d-flex flex-column">
+                <Card.Title className="package__item--title text-center font-weight-bold">
+                  ${checked ? pricingAnnuallyPro1 : packageList[0]?.pricing}
+                  <span className="package__item--sub">
+                    /{checked ? "year" : "month"}
+                  </span>
+                </Card.Title>
+                <Link
+                  className="package__item--btn text-center"
+                  to="/home/sign-up"
+                >
+                  Start free trial
+                </Link>
+                <Card.Text className="package__item--text">
+                  Inspire by Rod Tidwell’s word in the movie Jerry Maguire. “It
+                  means love, respect, community… and the dollars too. The
+                  entire package. The Quan”. We are your ambassador of Quan. We
+                  will show you the money
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          {/* <div className="package__col">
 
             <div className="package__item__group">
               <Card className="package__item">
-                {/* <Card.Img style={{ objectFit: 'contain' }} variant="top" src={Packkage1} /> */}
                 <HeaderPackage
                   title="Package"
                   subTitle=""
@@ -190,7 +330,7 @@ export default function Package() {
               </Card>
             </div>
 
-          </div>
+          </div> */}
         </Row>
 
         <div className="package__container package__feature_title">
